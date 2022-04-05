f4e2ac38cdee5ebc12251139d0d0de15225b26b4
jesse zenon <jesse.zenon@gmail.com>
2022-04-04 21:09:30 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`; 
+
+console.log(currentUser.indexOf(0));
+
