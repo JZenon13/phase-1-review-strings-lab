7e4dec06b9b72900779d3d3e16fd01da33f2f371
jesse zenon <jesse.zenon@gmail.com>
2022-04-25 14:52:02 -0500

    commit tree 71466130511ff2f9df08f207e21e7c9842eceb80

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // Write your code in this file!
-const currentUser = 'Grace Hopper';
+const currentUser = "Grace Hopper";
 
 let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 let excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
 
-let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`; 
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
 
-console.log(currentUser.indexOf(0));
-
+console.log(currentUser.slice(0, 3));
